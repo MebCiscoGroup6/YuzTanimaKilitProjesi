71857122362145feeca720d36d15290900c991e4
togius@hotmail.com <Megetol16>
2020-08-14 19:48:40 +0300

    14 Agustos 2020 7. güncelleme. Rapor tablosu bağlandı. Tarih text de sorgulama ayarı düzleltildi. Matplotlib eksik

--- report.py
+++ report.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import *
 from arayuzler.ui_report import Ui_Report
-from model.Veritabani_Kisi import VeriTabaniKisi,RaporTuru
+from model.Veritabani_Kisi import VeriTabaniKisi, RaporTuru
 
 
 class Report(QWidget):
@@ -24,19 +24,19 @@ class Report(QWidget):
 
     def sorguTurleriniGetir(self):
         self.ui.cmbSorguTuru.clear()
-        self.ui.cmbSorguTuru.addItem("Seçiniz","")
-        self.ui.cmbSorguTuru.addItem("Tek Tarih Sorgula",RaporTuru.TekTarih)
-        self.ui.cmbSorguTuru.addItem("Tarih Aralığı Sorgula",RaporTuru.TarihAraligi)
-        self.ui.cmbSorguTuru.addItem("Kişiye Göre Sorgula",RaporTuru.KisiyeGore)
+        self.ui.cmbSorguTuru.addItem("Seçiniz", "")
+        self.ui.cmbSorguTuru.addItem("Tek Tarih Sorgula", RaporTuru.TekTarih)
+        self.ui.cmbSorguTuru.addItem("Tarih Aralığı Sorgula", RaporTuru.TarihAraligi)
+        self.ui.cmbSorguTuru.addItem("Kişiye Göre Sorgula", RaporTuru.KisiyeGore)
 
     def clickSorgulamaBaslat(self):
         sorgulamaTuru = self.ui.cmbSorguTuru.currentData()
         if sorgulamaTuru == RaporTuru.TekTarih:
-            tarih = self.ui.leTarih1.text()
+            tarih = self.tarihDuzenle(self.ui.leTarih1.text())
             self.raporListesi = self.vtk.KisiRaporlari(raporTuru=RaporTuru.TekTarih, tarih=tarih)
         elif sorgulamaTuru == RaporTuru.TarihAraligi:
-            tarih1 = self.ui.leTarih1.text()
-            tarih2 = self.ui.leTarih2.text()
+            tarih1 = self.tarihDuzenle(self.ui.leTarih1.text())
+            tarih2 = self.tarihDuzenle(self.ui.leTarih2.text())
             self.raporListesi = self.vtk.KisiRaporlari(raporTuru=RaporTuru.TarihAraligi, tarih1=tarih1, tarih2=tarih2)
         elif sorgulamaTuru == RaporTuru.KisiyeGore:
             kisiId = self.ui.cmbKisiAdi.currentData()
@@ -45,19 +45,24 @@ class Report(QWidget):
         self.tabloOlustur()
         print(self.raporListesi)
 
-
     def tabloOlustur(self):
 
         satirSayisi = len(self.raporListesi)
-        sutunSayisi = 3 #gosterilecek sutun sayisi
+        sutunSayisi = 3  # gosterilecek sutun sayisi
         self.ui.tbwRaporlar.setRowCount(satirSayisi)
         self.ui.tbwRaporlar.setColumnCount(sutunSayisi)
         # #sutunlar
-        self.ui.tbwRaporlar.setHorizontalHeaderItem(0,QTableWidgetItem("Rapor Id"))
-        self.ui.tbwRaporlar.setHorizontalHeaderItem(1,QTableWidgetItem("Ad Soyad"))
-        self.ui.tbwRaporlar.setHorizontalHeaderItem(2,QTableWidgetItem("Tarih"))
+        self.ui.tbwRaporlar.setHorizontalHeaderItem(0, QTableWidgetItem("Rapor Id"))
+        self.ui.tbwRaporlar.setHorizontalHeaderItem(1, QTableWidgetItem("Ad Soyad"))
+        self.ui.tbwRaporlar.setHorizontalHeaderItem(2, QTableWidgetItem("Tarih"))
         # #satirlar
         for satir, rapor in enumerate(self.raporListesi):
-            self.ui.tbwRaporlar.setItem(satir,0, QTableWidgetItem(str(rapor.raporId)))
-            self.ui.tbwRaporlar.setItem(satir,1, QTableWidgetItem(rapor.adSoyad))
-            self.ui.tbwRaporlar.setItem(satir,2, QTableWidgetItem(rapor.tarih))
+            self.ui.tbwRaporlar.setItem(satir, 0, QTableWidgetItem(str(rapor.raporId)))
+            self.ui.tbwRaporlar.setItem(satir, 1, QTableWidgetItem(rapor.adSoyad))
+            self.ui.tbwRaporlar.setItem(satir, 2, QTableWidgetItem(rapor.tarih))
+
+    def tarihDuzenle(self, tarih):
+        tarih = str(tarih).replace(".", "-").replace("/", "-")
+        tmp = tarih.split("-")
+        tarih = tmp[2] + "-" + tmp[1] + "-" + tmp[0]
+        return tarih
